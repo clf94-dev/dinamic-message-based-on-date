3cfc6ed85a13917fedc92d770f9895a4c0a1a5ec
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-01 07:12:59 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const message = document.querySelector('div.output');
+const buttonClick = document.querySelector('button');
+
+buttonClick.addEventListener('click', getDate);
+let dinMessage;
+
+function getDate() {
+    const date = new Date();
+    var time = date.getHours();
+    if (time <= 6 && time >= 21) {
+        dinMessage = "It's night  time";
+        message.style.backgroundColor = "black";
+        message.style.color = "white";
+
+    } else if (time <= 12) {
+        dinMessage = "It's morning time";
+        message.style.backgroundColor = "lightblue";
+        message.style.color = "blue";
+    } else {
+        dinMessage = "It's afternoon";
+        message.style.backgroundColor = "darkorange";
+        message.style.color = "orange";
+    }
+    message.style.fontSize = "40px";
+    message.textContent = dinMessage;
+
+}
